18563f8a8d63a2425a53d2ec7bab62becf91d1a7
malhwiu <58634778+malhwiu@users.noreply.github.com>
2023-02-09 12:00:47 +0200

    form submit works

--- res/script.js
+++ res/script.js
@@ -1,10 +1,27 @@
 $(document).ready(
     function()
     {
-
+        $("form").on("submit", function (e) {
+            var dataString = $(this).serialize();
+            $.ajax({
+                type: "POST",
+                url: "http://localhost:8080/index.php",
+                data: dataString,
+                success: function () {
+                    console.log(dataString);
+                }
+            });
+        
+            e.preventDefault();
+        });
+        
+        $(`#submit-settings`).click(function (){
+            console.log("Click!");
+            $(`form#game-settings`).submit();
+        });
     }
 );
 
-function updateVol(val) {
-    $(``)
-}
+$(document).on('input change', '#vol', function() {
+    $('#volLabel').html($(this).val());
+});
